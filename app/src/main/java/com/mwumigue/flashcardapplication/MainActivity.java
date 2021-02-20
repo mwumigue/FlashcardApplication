f2cbbdd0c55dc7449df1c707e7bb1e5574661ac4
mwumigue <mwumigue@users.noreply.github.com>
2021-02-20 16:08:03 -0500

    Created Question and Answer textviews with their  respective setOnClickListener statements

--- app/src/main/java/com/mwumigue/flashcardapplication/MainActivity.java
+++ app/src/main/java/com/mwumigue/flashcardapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mwumigue.flashcardapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
